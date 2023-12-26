be1013def302658ceca92e43ebac6374e4daaca9
Edga380 <a.edgaras44@gmail.com>
2023-12-26 22:54:10 +0000

    Openai api connection established

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const conversationContainer = document.getElementById("conversationContainer");
+
 function toggle() {
   const menuBtnToggle = document.querySelector(".toggle-left-menu-button");
   menuBtnToggle.classList.toggle("left-menu-toggle-button-transition");
@@ -37,4 +39,59 @@ function closeAllMenusExcept(exceptMenu) {
       menu.style.display = "none";
     }
   });
+}
+
+// Send message
+async function sendMessage(message) {
+  const response = await fetch("/submit", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message }),
+  });
+
+  if (response.ok) {
+    const result = await response.json();
+    updateConversation(result.openaiResponse, "ChatGPT:");
+  } else {
+    console.error(
+      "Failed to send message:",
+      response.status,
+      response.statusText
+    );
+  }
+}
+
+// Update conversation and input html elements
+function updateConversation(message, username) {
+  conversationContainer.insertAdjacentHTML(
+    "beforeend",
+    `<div class="conver-user-container">
+        <div class="conver-user">
+        <img
+            src="${
+              username === "You"
+                ? "https://pics.craiyon.com/2023-06-25/840d68a3348b4f00b23af0cfc4ac71d2.webp"
+                : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/768px-ChatGPT_logo.svg.png"
+            }"
+            class="conversation-profile-img"
+        />
+        <div class="conver-username">${username}</div>
+        </div>
+        <div class="conver-text">
+            ${message}
+        </div>
+    </div>`
+  );
+}
+
+// Submit message to server
+function submitForm() {
+  const textarea = document.getElementById("resizableTextarea");
+  const message = textarea.value.trim();
+  if (message !== "") {
+    sendMessage(message);
+    updateConversation(message, "You");
+    // Optionally, clear the textarea or perform other UI updates
+    textarea.value = "";
+  }
 }
